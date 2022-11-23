8682370932da33872b853881f3420e856783ea06
Ivan Vecino <ivanveci28@gmail.com>
2022-11-23 15:30:48 -0300

    prueba 5.2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
     verificarInicioDeSesion();
-    
+
     google.accounts.id.initialize({
         client_id: "809127837215-6m5sscat51irktibf6mkd57gnv8s7r9v.apps.googleusercontent.com",
         callback: handleCredentialResponse,
         auto_select: false,
         auto: true
     });
+    google.accounts.id.renderButton(
+        document.getElementById("google-button"),
+        { theme: "filled_blue", size: "medium", width: '200' }
+    );
     
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
